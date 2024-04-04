2a3c18baf777e2e132f101073c165043f65a3416
Ary1993 <43453934+Ary1993@users.noreply.github.com>
2024-04-04 17:15:18 +0000

    feat:Wishes

--- src/front/js/pages/Wishes.jsx
+++ src/front/js/pages/Wishes.jsx
@@ -6,11 +6,11 @@ import { Context } from '../store/appContext';
 export const Wishes = () => {
     const { store, actions } = useContext(Context);
     return (
-        
+        !store.user ? <div>No t</div> :
         <div>
             <div className="card d-flex align-center" style={{ width: "18rem" }}>
                 <div className="card-body">
-                    <p className="card-text">ID: {store.Wishes.id}</p>
+                    <p className="card-text">ID: {store.wishes.id}</p>
                 </div>
             </div>
         </div>
