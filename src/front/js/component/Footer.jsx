9c29f48de95d89924fb70bed9da9b3ce33f54e2f
Carlos Betancourt <146739071+DonBeta@users.noreply.github.com>
2024-04-08 14:52:40 +0000

    feat: terminado el slider, pero queda por decidir o terminar de configurar de 3

--- src/front/js/component/Footer.jsx
+++ src/front/js/component/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => (
 
 	<footer className="footer mt-auto bg-body-tertiary text-center m-3">
 		<div className="text-center p-3">
-		<i className="fa-solid fa-gamepad"></i> <i className="fa-solid fa-earth-europe"></i> Trabajo del Grupo 1: Carlos Betancourt, Allexandre Pallas, Aitor Moyano, Pedro Martín   <i className="fa-brands fa-square-instagram"></i> <i className="fa-brands fa-facebook"></i>
+			<i className="fa-solid fa-gamepad"></i> <i className="fa-solid fa-earth-europe"></i>  Developers: <a href="https://github.com/DonBeta" target="_blank">Carlos Betancourt</a>,  <a href="https://github.com/AitorMoyano" target="_blank">Aitor Moyano</a>, <a href="https://github.com/Ary1993" target="_blank">Alexandre Pallas</a>, <a href="https://github.com/pmart1n" target="_blank">Pedro Martin</a>  <i className="fa-brands fa-square-instagram"></i> <i className="fa-brands fa-facebook"></i>
 		</div>
 	</footer>
 
